77f4528e01f9d0857bcd84e53b1058cbfa3dc620
paddym <paddy@fundrecs.com>
2018-01-10 16:03:58 +0000

    Added realName variable to Person domain and also seeded some bootstrap data upon running application.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,20 @@
+import org.grails.twitter.auth.*
+
 class BootStrap {
 
+    def springSecurityService
+
     def init = { servletContext ->
+        if(!Person.count()) {
+
+            def userRole = new Authority(authority: 'ROLE_USER').save()
+
+            def password = springSecurityService.encodePassword('password')
+
+            [jeff: 'jeff Brown', graeme: 'Graeme Rocher', burt: 'Burt Reynolds']
+                def user = new Person(username: userName, realName: realName)
+                PersonAuthority.create user, userRole, true
+        }
     }
     def destroy = {
     }
